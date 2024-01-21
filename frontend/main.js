d33bd487ded3174c8cd2c7ce5957d2e1366902a3
Falk Werner <falk.werner@gmx.net>
2024-01-21 12:14:33 +0100

    allow to filter by tags

--- frontend/main.js
+++ frontend/main.js
@@ -7,6 +7,7 @@ import { init_settings } from "./settings.js"
 import { FakeNoteProvider } from "./fakenoteprovider.js"
 import { NoteList } from "./notelist.js"
 import { Editor } from "./editor.js"
+import { TagList } from "./taglist.js"
 
 init_titlebar();
 init_settings();
@@ -15,9 +16,13 @@ slider_attach(document.querySelector("#slider"));
 const editor = new Editor();
 
 const noteProvider = new FakeNoteProvider();
+
+const taglist_elemnt = document.querySelector("#taglist");
+const taglist = new TagList(taglist_elemnt, noteProvider);
+
 const notelist_element = document.querySelector("#notelist");
 const filter_element = document.querySelector('#filter');
-const notelist = new NoteList(noteProvider, notelist_element, filter_element, editor);
+const notelist = new NoteList(noteProvider, notelist_element, filter_element, taglist, editor);
 document.querySelector("#add-note").addEventListener("click", async () => {
   notelist.add_new();
 })
