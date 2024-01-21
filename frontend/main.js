419c28d0c4cf42b3e4dfee2dfe1630f4df96242f
Falk Werner <falk.werner@gmx.net>
2024-01-21 10:19:19 +0100

    allow to filter notes

--- frontend/main.js
+++ frontend/main.js
@@ -16,7 +16,8 @@ const editor = new Editor();
 
 const noteProvider = new FakeNoteProvider();
 const notelist_element = document.querySelector("#notelist");
-const notelist = new NoteList(noteProvider, notelist_element, editor);
+const filter_element = document.querySelector('#filter');
+const notelist = new NoteList(noteProvider, notelist_element, filter_element, editor);
 document.querySelector("#add-note").addEventListener("click", async () => {
   notelist.add_new();
 })
