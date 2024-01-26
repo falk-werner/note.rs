9c8ccb954587b092eb0d52fe83b781cd3c7486e5
fersaru <91319844+fersaru@users.noreply.github.com>
2024-01-26 10:49:53 +0100

    Backend: list: shrink error handling
    * this follows [tauri's guide on error handling](https://tauri.app/v1/guides/features/command/#error-handling)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -22,35 +22,13 @@ async fn list() -> Result<Vec<String>, String> {
   let mut notes = Vec::<String>::new();
 
   // read all files in dir
-  match std::fs::read_dir(base_path) {
-    Ok(dir_entries) => {
-      for dir_entry in dir_entries {
-        match dir_entry {
-          Ok(dir_entry) => {
-            // filter for directories
-            match dir_entry.file_type() {
-              Ok(file_type) => {
-                if file_type.is_dir() {
-                  // add README.md to the found dir names and save them in list
-                  notes.push(Path::new(&dir_entry.file_name()).join("README.md").to_str().unwrap().to_string());
-                }
-              }
-              Err(e) => { 
-                // error while figuring out the type of each file in the dir
-                return Err(e.to_string())
-              }
-            }
-          }
-          Err(e) => { 
-            // error while reading each object in dir
-            return Err(e.to_string()) 
-          }
-        }
-      }
-    }
-    Err(e) => { 
-      // error while reading the dir
-      return Err(e.to_string())
+  let dir_entries = std::fs::read_dir(base_path).map_err(|err| return err.to_string())?;
+  for dir_entry in dir_entries {
+    let dir_entry = dir_entry.map_err(|err| return err.to_string())?;
+    let file_type = dir_entry.file_type().map_err(|err| return err.to_string())?;
+    if file_type.is_dir() {
+      // add README.md to the found dir names and save them in list
+      notes.push(Path::new(&dir_entry.file_name()).join("README.md").to_str().unwrap().to_string());
     }
   }
   return Ok(notes)
