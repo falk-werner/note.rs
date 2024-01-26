77904c67b19a96783bae90a6e1e132f7a3eb2150
fersaru <91319844+fersaru@users.noreply.github.com>
2024-01-26 20:51:36 +0100

    Backend: list: documentation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,7 +13,9 @@ fn main() {
     .expect("error while running tauri application");
 }
 
-/// list the names of the notes, which is the list of all directories in the base directory  that have a `README.md` file inside
+/// list the names of the notes
+/// 
+/// list all directories in the base directory that have a `README.md` file inside
 #[tauri::command]
 async fn list() -> Vec<String> {
   let base_path = Path::new(home_dir().unwrap().as_path()).join(".notes");
