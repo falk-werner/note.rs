d38b8a8ab098adfe76a0d35908c7d1a950ccf17b
fersaru <91319844+fersaru@users.noreply.github.com>
2024-01-26 17:25:49 +0100

    Backend: list: expand error handling to continue on error

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::path::Path;
-
 use tauri::api::path::home_dir;
 
 fn main() {
@@ -16,20 +15,39 @@ fn main() {
 
 /// list all possible README.md locations in dirs of the save dir
 #[tauri::command]
-async fn list() -> Result<Vec<String>, String> {
+async fn list() -> Vec<String> {
   let base_path = Path::new(home_dir().unwrap().as_path()).join(".notes");
-  
-  let mut notes = Vec::<String>::new();
-
+  let mut notes = Vec::<String>::new();  
   // read all files in dir
-  let dir_entries = std::fs::read_dir(base_path).map_err(|err| err.to_string())?;
-  for dir_entry in dir_entries {
-    let dir_entry = dir_entry.map_err(|err| err.to_string())?;
-    let file_type = dir_entry.file_type().map_err(|err| err.to_string())?;
-    if file_type.is_dir() {
-      // add README.md to the found dir names and save them in list
-      notes.push(Path::new(&dir_entry.file_name()).join("README.md").to_str().unwrap_or_default().to_string());
+  match std::fs::read_dir(base_path) {
+    Ok(dir_entries) => {
+      for dir_entry in dir_entries {
+        match dir_entry {
+          Ok(dir_entry) => {
+            match dir_entry.file_type() {
+              Ok(file_type) => {
+                if file_type.is_dir() {
+                  let readme_path = Path::new(&dir_entry.file_name()).join("README.md");
+                  if readme_path.as_path().exists() && readme_path.to_str().is_some() {
+                    notes.push(readme_path.to_str().unwrap_or_default().to_string())
+                  } else {
+                    error_handling(format!("{readme_path:?}"));
+                  }
+                }
+              }
+              Err(e) => { error_handling(e.to_string()); }
+            }
+          }
+          Err(e) => { error_handling(e.to_string()); }
+        }
+      }
     }
+    Err(e) => { error_handling(e.to_string()); }
   }
-  return Ok(notes)
+  return notes
+}
+
+fn error_handling(e: String) {
+  // implementation up to discussion
+  eprintln!("{e}");
 }
