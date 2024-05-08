954dd368c3942840c59cab3b5fc6b41ffd51c6db
Falk Werner <falk.werner@gmx.net>
2024-05-09 01:10:20 +0200

    fix slider logic
    - fix resizing  (snap to cursor position)
    - set user-select to "none" during resizing to prevent glitches due to unwanted text selection
    - change cursor to "col-resize" during resizing

--- frontend/slider.js
+++ frontend/slider.js
@@ -1,14 +1,28 @@
 const slider_attach = function (element) {
-    const target_name = element.getAttribute('data-slider-target');
-    const target = document.querySelector(`#${target_name}`);
+    const body = document.querySelector("body");
+    const left = element.previousElementSibling;
+    const right = element.nextElementSibling;
+    const minWidth = left.clientWidth;
 
-    const resize = (event) => {
-        target.style.flexBasis = `${event.x}px`;
+    const resize = (event) => {        
+        left.style.minWidth = `${Math.max(event.x, minWidth)}px`;
     };
 
     element.addEventListener("mousedown", () => {
+        left.style.userSelect = "none";
+        left.style.webkitUserSelect = "none";
+        right.style.webkitUserSelect = "none";
+        right.style.userSelect = "none";
+        body.style.cursor = "col-resize";
+
         document.addEventListener("mousemove", resize, false);
         document.addEventListener("mouseup", () => {
+            left.style.userSelect = null;
+            left.style.webkitUerSelect = null;
+            right.style.userSelect = null;
+            right.style.webkitUserSelect = null;
+            body.style.cursor = null;
+
             document.removeEventListener("mousemove", resize, false);
         }, false);
     }, false);
